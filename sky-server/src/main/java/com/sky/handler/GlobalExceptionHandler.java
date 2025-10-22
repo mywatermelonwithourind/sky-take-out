8dda939fcce1abc76e42e8759a3ebc93251918ca
itcast <1148537112@qq.com>
2025-10-22 15:52:51 +0800

    新增员工功能完善（添加SQL异常捕获）

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,22 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 用于捕获sql异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result doSQLException(SQLIntegrityConstraintViolationException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        if (ex.getMessage().contains("Duplicate")){
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2];
+            return Result.error(msg+ MessageConstant.ALREADY_EXIST);
+        }
+        return Result.error(MessageConstant.UNKNOWN_ERROR);
     }
 
 }
