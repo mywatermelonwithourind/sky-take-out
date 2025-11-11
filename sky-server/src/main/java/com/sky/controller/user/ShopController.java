12ff06aead171f384e88c331ef79a470ed32f113
itcast <1148537112@qq.com>
2025-11-11 20:45:00 +0800

    店铺营业状态设置功能实现

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,30 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@Api(tags = "店铺操作接口")
+@RequestMapping("/user/shop")
+@RestController("userShopController")
+public class ShopController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    private final static String KEY= "SHOP_STATUS";
+
+    @ApiOperation("获取店铺营业状态")
+    @GetMapping("/status")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer)redisTemplate.opsForValue().get(KEY);
+        log.info("店铺营业状态: {}"+(status==1?"营业":"打烊"));
+
+        return Result.success(status);
+    }
+}
