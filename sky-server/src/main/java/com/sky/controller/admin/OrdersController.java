d7dcf737a3dfa5beaf7c3cafef34323d61518d28
itcast <1148537112@qq.com>
2026-01-25 22:45:39 +0800

    管理端查询订单详情功能实现

--- sky-server/src/main/java/com/sky/controller/admin/OrdersController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrdersController.java
@@ -5,11 +5,13 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrdersService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +47,18 @@ public class OrdersController {
         log.info("各个状态的订单数量统计");
         OrderStatisticsVO vo=ordersService.statistics();
         return Result.success(vo);
+    }
+
+    /**
+     * 查询订单详情
+     * @return
+     */
+    @ApiOperation("查询订单详情")
+    @GetMapping("/details/{id}")
+    public Result<OrderVO> details(@PathVariable("id") Long id){
+        log.info("查询订单详情：id= {}", id);
+        OrderVO orderVO = ordersService.orderDetail(id);
+        return Result.success(orderVO);
+
     }
 }
